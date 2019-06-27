c187e4e02b25b57f148ae00cf84a1133770c11ff
Beatriz Tovar <beatrizctovar@gmail.com>
2019-06-27 14:58:16 -0700

    adding characters service

---
+++ src/services/mockCharacterService.js
@@ -0,0 +1,100 @@
+const characters = [
+  {
+    id: 1,
+    src: "./images/characters/AmyWong.png",
+    title: "Amy Wong",
+    description: "Character Description Here"
+  },
+  {
+    id: 2,
+    src: "./images/characters/Bender.png",
+    title: "Bender B. Rodriguez",
+    description: "Character Description Here"
+  },
+  {
+    id: 3,
+    src: "./images/characters/Fry.png",
+    title: "Phillip J. Fry",
+    description: "Character Description Here"
+  },
+  {
+    id: 4,
+    src: "./images/characters/HermesConrad.png",
+    title: "Hermes Conrad",
+    description: "Character Description Here"
+  },
+  {
+    id: 5,
+    src: "./images/characters/Hypnotoad.png",
+    title: "Hypnotoad",
+    description: "Character Description Here"
+  },
+  {
+    id: 6,
+    src: "./images/characters/KifKroker.png",
+    title: "Kif Kroker",
+    description: "Character Description Here"
+  },
+  {
+    id: 7,
+    src: "./images/characters/Leela.png",
+    title: "Turanga Leela",
+    description: "Character Description Here"
+  },
+  {
+    id: 8,
+    src: "./images/characters/Mom.png",
+    title: "Mom",
+    description: "Character Description Here"
+  },
+  {
+    id: 9,
+    src: "./images/characters/Nibbler.png",
+    title: "Nibbler",
+    description: "Character Description Here"
+  },
+  {
+    id: 10,
+    src: "./images/characters/Nixon.png",
+    title: "Nixon",
+    description: "Character Description Here"
+  },
+  {
+    id: 11,
+    src: "./images/characters/ProfessorFarmsworth.png",
+    title: "Professor Farmsworth",
+    description: "Character Description Here"
+  },
+  {
+    id: 12,
+    src: "./images/characters/Roberto.png",
+    title: "Roberto",
+    description: "Character Description Here"
+  },
+  {
+    id: 13,
+    src: "./images/characters/RobotDevil.png",
+    title: "Robot Devil",
+    description: "Character Description Here"
+  },
+  {
+    id: 14,
+    src: "./images/characters/Scruffy.png",
+    title: "Scruffy",
+    description: "Character Description Here"
+  },
+  {
+    id: 15,
+    src: "./images/characters/ZappBraniggan.png",
+    title: "Zapp Braniggan",
+    description: "Character Description Here"
+  },
+  {
+    id: 16,
+    src: "./images/characters/Zoidberg.png",
+    title: "Zoidberg",
+    description: "Character Description Here"
+  }
+];
+
+export default characters;
