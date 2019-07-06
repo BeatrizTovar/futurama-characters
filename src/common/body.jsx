6bd8617bd2dbfa6f6e440656a69b3e85e6def9b1
Beatriz Tovar <beatrizctovar@gmail.com>
2019-07-06 00:17:13 -0700

    creating card layout

--- src/common/body.jsx
+++ src/common/body.jsx
@@ -6,22 +6,20 @@ import { Box, Image, Stack } from "grommet";
 const Body = ({ characters }) => {
   // debugger;
   return (
-    // <Stack>
     <Fragment>
-      <div className="hqBack">
-        <section className="card">
-          {characters.map(char => (
-            <div className="card-content" key={char.id}>
-              <Image src={char.src} />
-              <p>{char.title}</p>
-              <p>{char.desc}</p>
-              <p>{char.src}</p>
-              {/* {console.log(typeof Amy)} */}
-            </div>
-          ))}
-        </section>
-        {/* </Stack> */}
-      </div>
+      <Stack>
+        <div className="hqBack">
+          <section className="card">
+            {characters.map(char => (
+              <div className="card-content" key={char.id}>
+                <Image src={char.src} />
+                <p>{char.title}</p>
+                <p>{char.desc}</p>
+              </div>
+            ))}
+          </section>
+        </div>
+      </Stack>
     </Fragment>
   );
 };
