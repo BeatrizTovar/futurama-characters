8306e7d9f435b98ec4aabd224f64c5d6cd7d61bf
Beatriz Tovar <beatrizctovar@gmail.com>
2019-07-22 13:39:33 -0700

    adding comments for future changes

--- src/services/characterQuotes.js
+++ src/services/characterQuotes.js
@@ -10,12 +10,19 @@ function quote(character) {
 }
 
 const successResponse = response => {
-  let data = response.data;
-  console.log("D", data);
+  let data = response.data; // save each characters data in an array
+
+  // array of objects each one containing a characters, quote, image,
+  // character => id , quote => quote, image is not currently used
+
+  // need: sort through array and return only 1 entry at random
+  // check length of array for more than one entry if more less than one
+  // return default message
+
   const result = response.data.forEach(q => console.log(q.quote));
   // console.log(result)
   // console.log("SR: ", response.data.filter(character => character.quote));
-  return response.data;
+  // return response.data;
 };
 
 const errorResponse = response => {
