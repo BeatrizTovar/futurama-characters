aa8980315d93b04be47060241fd697673149b822
Beatriz Tovar <beatrizctovar@gmail.com>
2019-07-24 22:23:50 -0700

    wip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,11 +16,16 @@ class Characters extends Component {
   }
 
   getCharacterQuote(character) {
+    // Option 1
     // need to read the quote based on the character which comes
     // the characterQuote service
     // needs to be called after CMD which adds the names to the object
     // once that;s set need to read per name and
     // add quote property to each character object
+
+    // Option 2
+    // obtain quotes from second api and do the mapping and matching
+    // in the body just as the mapping of all characters
 
     // characterQuote returns a promise
     // need to resolve promise
--- src/services/characterQuotes.js
+++ src/services/characterQuotes.js
@@ -22,7 +22,8 @@ const successResponse = response => {
   } else {
     //  data[i] where i is a random integer based on array length
     let randomIndex = data[Math.floor(Math.random() * length)];
-    // console.log(randomIndex);
+    console.log(randomIndex);
+    debugger
     return randomIndex;
   }
 };
